a974e3f188e78777eedcd7d4e3e90f4ecf7b1b27
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-12-18 13:20:56 -0600

    add gamesReducers to All

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,14 @@
 import { combineReducers } from 'redux'
+import gamesReducer from './gamesReducer'
 // import all individual reducers below:
 
 
-
-
-// combineReducers is a hook? method that combines all reducers for export and 
+// combineReducers is a redux method that combines all reducers for export and 
 // allows you to rename the main reducer that handles multiple cases.
 
 // allReducers are imported to the MAIN index.js file and added as the 1st argument to the createStore method. 
 const allReducers = combineReducers({
-
+    gamesReducer: gamesReducer
 })
 
 export default allReducers
