0f5b0d92ccae88566115b6d3b67947f51230d825
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-12-18 13:42:19 -0600

    init/build gamesReducer

---
+++ src/reducers/gamesReducer.js
@@ -0,0 +1,27 @@
+import { renderGames, RENDER_GAMES } from '../actions'
+
+
+const INITIAL_STATE = {hello: "hey, Ry!", storeGames: []}
+const gamesURL = "http://localhost:3000/games"
+
+const gamesReducer = (state = INITIAL_STATE, action) => {
+    switch(action.type) {
+        case "GET_GAMES":
+            return {...state, storeGames: [...state.storeGames, action.payload]}
+            console.log(action.payload)
+        // case "LOAD_GAMES":
+        //     return {...state, storeGames: [...state.storeGames, action.payload]}
+        //     console.log(state.gamesReducer)
+
+        default:
+            return state
+    }
+}
+
+// export const loadGames = () => async (dispatch, useState) => {
+//     const games = await fetch(gamesURL)
+//     .then(resp => resp.json())
+//     dispatch(renderGames(games))
+// }
+
+export default gamesReducer
