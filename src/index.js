dd9776a5d0a51f999d996fe74db1b29a34f7dae3
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-12-18 13:23:30 -0600

    define composeEnhancers in index.js

--- src/index.js
+++ src/index.js
@@ -2,20 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import allReducers from './reducers'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom' 
 import thunk from 'redux-thunk'
 
 //STORE -> GLOBALIZED STATE (where all our State data is kept)
-const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(allReducers, composeEnhancers(applyMiddleware(thunk)))
+                                                        // redux-thunk ^^^ ??
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
