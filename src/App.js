d1a6a6b0135c8807223a5baba831365dbf1b5a0d
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-12-18 13:22:53 -0600

    create imports and testings for App page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import './App.css';
-import GamesIndex from './components/GamesIndex'
-
+import GamesContainer from './components/GamesContainer'
+import { useSelector, useDispatch } from 'react-redux'
+import { getGames } from './actions'
+import { useEffect } from 'react'
+import Navbar from './components/Navbar';
+import SearchContainer from './components/SearchContainer'
+import Footer from './components/Footer'
+import loadGames from './reducers/gamesReducer'
 
 function App() {
+
+  const hello = useSelector(state => state.gamesReducer.hello)
+  console.log(hello)
+  const dispatch = useDispatch()
+  const getTheGames = useSelector(state => state.gamesReducer.storeGames)
+  console.log(getTheGames)
+
+  // const gamesURL = "http://localhost:3000/games"
+
+  const fetchGames = () => {
+   dispatch(getGames)
+  }
+  useEffect(() => {
+    fetchGames()
+  }, [fetchGames])
+
+  useEffect(() => {
+    console.log(getTheGames)
+  }, [getTheGames])
+
+  // const onLoad = () => {
+  //   dispatch(loadGames())
+  // }
+
   return (
     <div className="App">
-      <h1>BOARD HORDE</h1>
-      <GamesIndex />
+      <Navbar />
+      <h1>HORDE the BOARD </h1>
+      {/* <button onClick={ onLoad }>LOAD GAMES</button> */}
+      <GamesContainer/>
+      <SearchContainer />
+      <Footer />
     </div>
   );
 }
