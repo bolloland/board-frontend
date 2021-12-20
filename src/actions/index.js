1eebbaceccbf74a06484df1a6ccb027a35f37340
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-12-19 21:05:39 -0600

    create fetchGames funct

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,35 +6,55 @@
 //          payload: amount
 //     }
 // }
+// thunk allows us to create a function that 
+// passes dispatch into the results of the fetch
+
+
+export const fetchGames = () => {
+    return dispatch => fetch("http://localhost:3000/games")
+    .then(resp => resp.json())
+    .then(games => dispatch({type: "FETCH_GAMES", payload: games })
+    )
+}
+
+
+
+
+
+
+
+
+
+
 
 const gamesURL = "http://localhost:3000/games"
 
 export const RENDER_GAMES = "RENDER_GAMES"
 
-export const renderGames = (games) => ({
-    type: "RENDER_GAMES",
-    payload: games,
-})
+// export const renderGames = (games) => ({
+//     type: "RENDER_GAMES",
+//     payload: games,
+// })
 
-// b/c it's asynch, we need thunk to intercept the request and thunk will dispatch it when complete
-export const getGames = () => (dispatch) => {
-    fetch(gamesURL)
-    .then(resp => resp.json())
-    .then(allGames => {
-      dispatch({                // sends to my reducer
-          type: "GET_GAMES",
-          payload: allGames
-      })
-    })
-}
+// // b/c it's asynch, we need thunk to intercept the request and thunk will dispatch it when complete
+// export const getGames = () => (dispatch) => {
+//     fetch(gamesURL)
+//     .then(resp => resp.json())
+//     .then(allGames => {
+//       dispatch({                // sends to my reducer
+//           type: "GET_GAMES",
+//           payload: allGames
+//       })
+//     })
+// }
 
-export const loadGames = () => async (dispatch, getState) => {
-    const games = await fetch(gamesURL)
-    .then(resp => resp.json())
-    .then(storeGames => {
-      dispatch({                // sends to my reducer
-          type: "GET_GAMES",
-          payload: storeGames
-      })
-    })
-}
+// export const loadGames = () => async (dispatch, getState) => {
+//     const games = await fetch(gamesURL)
+//     .then(resp => resp.json())
+//     .then(storeGames => {
+//       dispatch({                // sends to my reducer
+//           type: "GET_GAMES",
+//           payload: storeGames
+//       })
+//     })
+// }
