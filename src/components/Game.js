6722a43b7c262e46cde8339ce03279a48ebef984
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-12-22 13:07:48 -0600

    update Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { addToCollection } from '../actions'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
-const Game = () => {
+const Game = ({gamedata}) => {
+    // gamedata is each game
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const myCollection = useSelector(state => state.gamesReducer.myCollection)
+
+    
+    let collected = myCollection.find(g => g.id == gamedata.id)
+    
+
     return (
-        <div>
-            
+        <div className="game-container">
+            {gamedata.name}<br />
+
+            <img onClick={() => { navigate(`/${gamedata.id}`) }} className="thumb" src={gamedata.thumb_url} alt="" />
+
+            Average Rating: <br />
+           {gamedata.avg_rating ? gamedata.avg_rating + " / 5": "n/a"}
+    
+    
+            {!collected ? <button onClick={() => dispatch(addToCollection(gamedata))}>add to collection</button> : <button>"in your collection"</button>}
+           
+           
+           <button>{gamedata.review_count == 0 ? "write a review" : "see " + gamedata.review_count + " review(s)"}</button>
+       
         </div>
     )
 }
