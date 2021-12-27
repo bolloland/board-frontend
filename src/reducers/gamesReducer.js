da3ff3746bde48e5c33dad6696bdb98461a7608d
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-12-26 22:54:37 -0600

    submitNewGame reducer + show/hide review form

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,25 +1,28 @@
-import { LOAD_GAMES, ADD_COLLECT } from "../actions/types"
+import { LOAD_GAMES, ADD_COLLECT, SUBMIT_NEW_GAME, SHOW_HIDE_REVIEW_FORM } from "../actions/types"
 
 const INITIAL_STATE = {
     storeGames: [],
-    myCollection: []
+    myCollection: [],
+    showHideReviews: [false]
 }
 
 // const gamesURL = "http://localhost:3000/games"
+// BLOG POST!!   LoadGames vs. SubmitNewGame
+//redux version of "push"
 
 export const gamesReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
         case LOAD_GAMES:
-            return {...state, storeGames: [...state.storeGames, action.payload]}
+            return {...state, storeGames: action.payload}
         case ADD_COLLECT:
-            console.log(action.payload)
             return {...state, myCollection: [...state.myCollection, action.payload]}
+        case SUBMIT_NEW_GAME:
+            return {...state, storeGames: [...state.storeGames, action.payload]}
+        case SHOW_HIDE_REVIEW_FORM:
+            return {...state, showHideReviews: [...state.showHideReviews, action.payload]}
         default:
-            return {...state}
-              
+            return {...state}      
     }
 }
-
-
 
 export default gamesReducer
