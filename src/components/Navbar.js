34553870f3f1dff953943dd4b738cdb0d81d28ae
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-12-26 22:53:20 -0600

    update NavBar for add-game and search functions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { useNavigate, Routes, Route } from 'react-router'
-import Page1 from '../pages/Page1'
-import Page2 from '../pages/Page2'
 import "../styles.css"  
 import MyCollection from './MyCollection'
 
@@ -15,8 +13,8 @@ const Navbar = () => {
             <h5>NAVBAR</h5>
         <button onClick={() => { navigate("/") }}>HOME</button>
         <button onClick={() => { navigate("/myCollection") }}>My Collection</button>
-        <button onClick={() => { navigate("/page3") }}>Add A Game</button>
-        <button onClick={() => { navigate("/page4") }}>PAGE4</button>
+        <button onClick={() => { navigate("/add-game") }}>Add A Game</button>
+        <button onClick={() => { navigate("/search") }}>SEARCH</button>
         </div>
     )
 }
