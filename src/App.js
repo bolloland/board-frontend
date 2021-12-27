9f40ff3807cd2a9a790436067d86a4f46fd8f3e7
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-12-26 22:50:11 -0600

    import AddGame and ShowGame components

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ import SearchContainer from './components/SearchContainer'
 import Footer from './components/Footer'
 import { fetchGames } from './actions';
 import MyCollection from './components/MyCollection';
+import AddGame from './components/AddGame';
+import ShowGame from './components/ShowGame';
 
 function App() {
 
-  
-  const getTheGames = useSelector(state => state.gamesReducer.storeGames)
-  console.log(getTheGames)
+  // const getTheGames = useSelector(state => state.gamesReducer.storeGames)
+  // console.log(getTheGames, "getting the Games")
   const dispatch = useDispatch()
 
   // as App loads, sudo DomContentLoad, says 
@@ -26,9 +27,10 @@ function App() {
     <div >
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<GamesContainer games={getTheGames} />} />
+        <Route exact path="/" element={<GamesContainer />} />
         <Route exact path="/myCollection" element={ <MyCollection  />}  />
-        {/* <Route exact path="/page2" element={ <Page2  />}  /> */}
+        <Route exact path="/add-game" element={ <AddGame />}  />
+        <Route exact path="/games/:id" element={ <ShowGame />}  />
       </Routes>
       <h1>HORDE the BOARD </h1>
       
