6cc9953e17775b2ccee41622e4e57eec3472de1f
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-12-30 03:18:32 -0600

    build out ShowGame

--- src/components/ShowGame.js
+++ src/components/ShowGame.js
@@ -2,42 +2,57 @@ import React from 'react'
 import { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { showHideReviewForm } from '../actions'
+import ReviewForm from './ReviewForm'
 import Review from './Review'
+import { useEffect } from 'react'
+
 
 const ShowGame = () => {
+    
     const pathname = (window.location.pathname.split("/"))
-    const slug = pathname[pathname.length-1]
+    const pathID = pathname[pathname.length-1]
+    // const slug = pathname[pathname.length-1]
     const games = useSelector(state => state.gamesReducer.storeGames)
-    const showHide = useSelector(state => state.gamesReducer.storeGames)
+    
+    const showHide = useSelector(state => state.gamesReducer.showHideReviews)
     const dispatch = useDispatch()
 
         const [showRevForm, setShowRevForm] = useState(false)
+        // LATER: change text on buttonClick?
         const revealForm = () => setShowRevForm(!showRevForm)
-        console.log(showRevForm, "review form status")
+            
 
-    let game = games.find(g => g.name === slug)
-    console.log(game, "game")
+    // let game = games.find(g => g.name === slug)
+    let game = games.find(g => g.id == pathID)
+    
+    const replaceHTML = (str) => {
+        return str.replace(/<[^>]*>?/gm, '')
+    }
+    const replacequotes = (str) => {
+        return str.replace(/(&quot\;)/gm,"\"")
+    }
 
-    let fixedDescription = game.description.replace(/<\/?[^>]+(>|$)/g, '')
-    // .gsub("&quot;", " ")
+    useEffect(() => {
+        
+    }, [games])
 
     return (
         <div className="one-game">
-            <h1> {game && game.name} ({game.year_published})</h1>
+            <h1> {game && game.name} ({game && game.year_published})</h1>
             <img className="thumb-show" src={game && game.thumb_url} alt=""  />
             
-            <div className="description-container">{fixedDescription }</div>
+            <div className="description-container">{game && replacequotes(replaceHTML(game.description))}</div>
             <br></br><br></br>
 
             <button>Add To Collection</button><br></br><br></br>
 
-            Rating: {game.avg_rating} / 5 <br></br><br></br>
+            Rating: {game && game.avg_rating} / 5 <br></br><br></br>
 
-            <button onClick={revealForm}>Add A Review - render review form </button>
-            {showRevForm ? <Review /> : !showRevForm }
+            <button onClick={revealForm}>Add A Review  </button>
+            {showRevForm ? <ReviewForm game={game}/> : !showRevForm }
 
-            Reviews To Date: {game.review_count}<br></br>
-            game.reviews.map(r =Review review=r )
+            Reviews To Date: {game && game.reviews.length}<br></br>
+            {game && game.reviews.map(rev => <Review rev={rev} />)}
             
         </div>
     )
