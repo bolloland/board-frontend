05ca41e83be0ea8fc62e1d00c399be9365b80fee
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-12-30 03:15:08 -0600

    set review_count to 0

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,32 +1,26 @@
-import { LOAD_GAMES, SUBMIT_NEW_GAME, ADD_COLLECT, SHOW_HIDE_REVIEW_FORM } from "./types"
+import { LOAD_GAMES, SUBMIT_NEW_GAME, ADD_COLLECT, SHOW_HIDE_REVIEW_FORM, SUBMIT_NEW_REVIEW, LOAD_REVIEWS } from "./types"
 import { useNavigate } from "react-router-dom"
-
-
 
 // ACTION -> describes WHAT you want to do, 
 // where we name it using "type:" || **function that returns an object**
 // export const increment = (amount) => {
 //     return {
-//         type: 'INCREMENT'   //increment() when used in onClick{}
-//          payload: amount
-//     }
-// }
+//         type: 'INCREMENT'   //increment() when used in onClick{}      }
 
-// // b/c it's asynch, we need thunk to intercept the request and thunk will dispatch it when complete
+//**b/c it's asynch, we need thunk to intercept the request and thunk will dispatch it when complete
 // ASYNCH FORMAT - BLOG POST
 export const fetchGames = () => (dispatch) => {
     fetch("http://localhost:3000/games")
     .then(resp => resp.json())
     .then(allGames => {
-      console.log(allGames, "allGames")
+        allGames.forEach(game => game.review_count = 0)
+      
         dispatch({                // sends to my reducer
           type: LOAD_GAMES,
           payload: allGames
         })
     })
 }
-
-export const showHideReviewForm = () => ({type: SHOW_HIDE_REVIEW_FORM, payload: true})
 
 export const submitNewGame = (inputs) => (dispatch) =>{
 
@@ -40,14 +34,32 @@ export const submitNewGame = (inputs) => (dispatch) =>{
   })
   .then(resp => resp.json())
   .then(newGameData => {
+    
     // console.log(newGameData, "newGameData")
     dispatch({                // sends to my reducer
       type: SUBMIT_NEW_GAME,
       payload: newGameData
     })
-    
 })
+}
 
+export const submitNewReview = (reviewData) => (dispatch) =>{
+  fetch(`http://localhost:3000/reviews`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(reviewData)
+  })
+  .then(resp => resp.json())
+  .then(newReviewData => {
+    console.log(newReviewData, "newReviewData")
+    dispatch({                // sends to my reducer
+      type: SUBMIT_NEW_REVIEW,
+      payload: newReviewData
+    })
+})
 }
 
 export const addToCollection = (item) => ({type: ADD_COLLECT, payload: item})
