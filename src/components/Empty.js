8b0c573914ea1b8de024f5fb4a8ceb71f49ffd46
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-01-01 17:25:02 -0600

    init create Empty container

---
+++ src/components/Empty.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Empty = () => {
+    return (
+        <div className="empty">
+        <br></br>
+            Oh no! You don't have any games in your collection! <br></br>
+            Use Search or look through the Game Rack to find some games. 
+        </div>
+    )
+}
+
+export default Empty
