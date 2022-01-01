c26e8aa13d224d5b725638e49c769b955f1cfaa0
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-01-01 17:23:31 -0600

    edit AddToCollect container

--- src/components/AddToCollectionButton.js
+++ src/components/AddToCollectionButton.js
@@ -5,14 +5,14 @@ import { addToCollection } from '../actions'
 const AddToCollectionButton = ({gamedata}) => {
 
     const dispatch = useDispatch()
-    const myCollection = useSelector(state => state.gamesReducer.myCollection)
 
+    const myCollection = useSelector(state => state.gamesReducer.myCollection)
     let collected = myCollection.find(g => g.id === gamedata.id)
 
     return (
         <div className="add-to-collection-button">
 
-        {!collected ? <button onClick={() => dispatch(addToCollection(gamedata))}>add to collection button TOO!</button> : <button>"in your collection"</button>}
+        {!collected ? <button className="button" onClick={() => dispatch(addToCollection(gamedata))}>add to collection (new)</button> : <button className="button" >in your collection</button>}
         
         </div>
     )
