1e4004cb9a219fc240062d4f15791b23eabfd7f6
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-01-01 17:26:06 -0600

    message for no games in container

--- src/components/MyCollection.js
+++ src/components/MyCollection.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Game from './Game'
+import Empty from './Empty'
 
 const MyCollection = (props) => {
 // taking in the myCollection array from state
     const dispatch = useDispatch()
     const collection = useSelector(state => state.gamesReducer.myCollection)
-    console.log(collection)
+    const empty = collection.length == 0
+    // console.log(empty)
 
     return (
-        <div>
-           <h2> MY COLLECTION! </h2>
-           {collection && collection.map(game => <Game gamedata={game}/>)}
+        <div >
+           <div className='collection-title'> MY COLLECTION! </div>
+           {/* <div className="collection-container">{collection && collection.map(game => <Game gamedata={game}/>)}</div> */}
+           <div className="collection-container">{empty? <Empty /> : collection && collection.map(game => <Game gamedata={game}/>)}</div>
         </div>
     )
 }
