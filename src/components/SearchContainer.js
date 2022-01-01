806a59949c3ba98995fe214554a00f45daf5f205
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-01-01 14:29:19 -0600

    SearchContainer renders w/o submit

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -4,40 +4,46 @@ import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux'
 import { useEffect } from 'react'
+import Game from './Game'
 
 const SearchContainer = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    const searchField = useSelector(state => state.gamesReducer.storeGames)
-    console.log(searchField)
+    const allgames = useSelector(state => state.gamesReducer.storeGames)
+    
     const [searchData, setSearchData] = useState("");
-
+    
+    let gameNames = allgames.filter(g => g.name.toLowerCase().includes(searchData.toLowerCase()) )
+        // console.log(gameNames)
+        // setFoundGames(gameNames)
+    
     const handleChange = (event) => {
         // const name = event.target.name;
         const value = event.target.value;
         // console.log(value)
         setSearchData(value)
     }
-    
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // dispatch(submitNewSearch(searchData))
-        
-        
-        alert("Searching...!");
-    }
+    // console.log(searchData)
+    console.log(gameNames)
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     // submitNewSearch(searchData)
+    //     alert("Searching...!");
+    // }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form>
+        {/* // <form onSubmit={handleSubmit}> */}
         <div className="search-field">
-            <h2>search container</h2>
+            <h2>Search Our Games!</h2>
             <input 
                 type="text"
                 placeholder="Search..."
-                value={searchData}
+                value={searchData || ""}
                 onChange={handleChange}
             />
-            <input type="submit" />
+            {gameNames.map(game => <Game gamedata={game}/>)}
+
         </div>
         </form>
     )
