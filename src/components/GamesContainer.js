817bd80ae289c1b5bfc840f8916b45474f273046
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-01-03 22:30:33 -0600

    create filter button

--- src/components/GamesContainer.js
+++ src/components/GamesContainer.js
@@ -3,24 +3,44 @@ import '../styles.css'
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import Game from './Game'
+import { useState } from 'react'
+
 
 const GamesContainer = () => {
     
     const games = useSelector(state => state.gamesReducer.storeGames)
     // aka mapStateToProps ^^
 
+    const [ratedGames, setRatedGames] = useState(false)
     
     useEffect(() => {
         console.log(games, "game update")
     }, [games])
+    
+    // const highRate = games.filter(game => game.avg_rating > 4) 
+
+    const handleOnclick = (event) => {
+        event.preventDefault()
+        setRatedGames(!ratedGames)
+    }
+
+    const filterGames = (games) => {
+        if (ratedGames) {
+            return games.filter(game => game.avg_rating > 4) 
+        } else {
+           return games
+        }
+    }
 
     return (
+        <div> <button className="button" onClick={handleOnclick}>showgames w/ rating over 4</button>
         <div className="allgames-container">
+         
          {/* && serves as an if>then statement */}
-         {games.map(game => <Game gamedata={game}/>)}
+         {filterGames(games).map(game => <Game game={game}/>)}
+         {/* {games.map(game => <Game gamedata={game}/>)} */}
 
-
-        </div>
+        </div></div>
     )
 }
 
